2e9e68e8443e88102d1512fd56bd5830e8306e6e
Xwilarg <xwilarg@yahoo.fr>
2020-10-08 11:22:07 +0200

    Add tag loading

--- Sanara-PreloadBooruQuizz/Program.cs
+++ Sanara-PreloadBooruQuizz/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BooruSharp.Booru;
+using BooruSharp.Search.Tag;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Sanara_PreloadBooruQuizz
@@ -7,8 +12,58 @@ namespace Sanara_PreloadBooruQuizz
     {
         static async Task Main()
         {
-            Console.ReadKey();
-            Console.WriteLine("Press any key to continue...");
+            // ---------- PARAMETERS
+
+            var booru = new Sakugabooru(); // Booru used for the search
+            var targetTag = TagType.Copyright; // What tag we try to find
+            var stopAfterFirst = true; // Do we stop searching after finding the tag
+
+            // ----------
+
+            var alreadyTaken = new List<int>();
+            var allData = new Dictionary<string, int>();
+            var tagsCache = new Dictionary<string, TagType>();
+
+            while (true)
+            {
+                var results = await booru.GetRandomPostsAsync(int.MaxValue);
+
+                foreach (var post in results)
+                {
+                    Console.Clear();
+                    Console.WriteLine($"Data loaded: {alreadyTaken.Count}\nAnime loaded: {allData.Keys.Count}\nPlease wait...");
+
+                    if (alreadyTaken.Contains(post.id))
+                        continue;
+                    alreadyTaken.Add(post.id);
+
+                    foreach (var tag in post.tags)
+                    {
+                        TagType type;
+                        if (!tagsCache.ContainsKey(tag))
+                        {
+                            var tagInfo = await booru.GetTagAsync(tag);
+                            tagsCache.Add(tag, tagInfo.type);
+                            type = tagInfo.type;
+                        }
+                        else
+                            type = tagsCache[tag];
+
+                        if (type == targetTag)
+                        {
+                            if (!allData.ContainsKey(tag))
+                                allData.Add(tag, 1);
+                            else
+                                allData[tag]++;
+
+                            if (stopAfterFirst)
+                                break;
+                        }
+
+                        File.WriteAllLines("TagsResult.txt", tagsCache.Select(x => x.Key + " " + x.Value));
+                    }
+                }
+            }
         }
     }
 }
