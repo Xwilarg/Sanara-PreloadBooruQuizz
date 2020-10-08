aeec8843bd6edbe66bf4882e7b367fe9ec2c0bca
Xwilarg <xwilarg@yahoo.fr>
2020-10-08 10:48:50 +0200

    Add empty project

---
+++ Sanara-PreloadBooruQuizz/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Sanara_PreloadBooruQuizz
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            Console.ReadKey();
+            Console.WriteLine("Press any key to continue...");
+        }
+    }
+}
